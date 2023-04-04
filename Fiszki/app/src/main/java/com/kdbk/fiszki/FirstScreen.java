1585230325bfc05eab950f83d72d0ad416db224a
DwiN3 <dwinicht333@gmail.com>
2023-04-04 19:43:10 +0200

    Dodanie drugiej strony

--- Fiszki/app/src/main/java/com/kdbk/fiszki/FirstScreen.java
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/FirstScreen.java
@@ -2,13 +2,37 @@ package com.kdbk.fiszki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstScreen extends AppCompatActivity {
+
+    private Button login,create,reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
+        setID();
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPasswordReset();
+            }
+        });
+    }
+
+    public void openPasswordReset(){
+        Intent intent = new Intent(this, PasswordReset.class);
+        startActivity(intent);
+    }
+
+    public void setID() {
+        login = findViewById(R.id.buttonLogin);
+        create = findViewById(R.id.buttonCreate);
+        reset = findViewById(R.id.buttonPasswordReset);
     }
 }
