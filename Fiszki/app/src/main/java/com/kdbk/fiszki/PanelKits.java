aed9bc3120eb7433c4f6010a5bb47319fc5c95b9
DwiN3 <dwinicht333@gmail.com>
2023-04-06 17:19:14 +0200

    Nie powoduje już problemów

--- Fiszki/app/src/main/java/com/kdbk/fiszki/PanelKits.java
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/PanelKits.java
@@ -1,14 +1,42 @@
 package com.kdbk.fiszki;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class PanelKits extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class PanelKits extends AppCompatActivity implements SelectListenerKits{
+
+    NextActivity nextActivity = new NextActivity(this);
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel_kits);
+        ArrayList<ModelKits> modelKitsArray = new ArrayList<>();
+        modelKitsArray.add(new ModelKits("Zestaw 1","ILOSC FISZEK", "30"));
+        modelKitsArray.add(new ModelKits("Zestaw 2","ILOSC FISZEK", "18"));
+        modelKitsArray.add(new ModelKits("Zestaw 3","ILOSC FISZEK", "25"));
+        modelKitsArray.add(new ModelKits("Zestaw 4","ILOSC FISZEK", "30"));
+        modelKitsArray.add(new ModelKits("Zestaw 5","ILOSC FISZEK", "11"));
+
+        mRecyclerView = findViewById(R.id.kitsPanelRecycleView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new AdapterKit(modelKitsArray, this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onItemClicked(ModelKits modelKits) {
+        Toast.makeText(this, modelKits.getTextNumberKit(), Toast.LENGTH_LONG).show();
     }
 }
