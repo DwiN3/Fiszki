2051bfa4e3767cefb6487c0778f501a75157724f
DwiN3 <dwinicht333@gmail.com>
2023-04-06 15:34:32 +0200

    Przygotowanie do recycleView od lewej do prawej

---
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/PanelKits.java
@@ -0,0 +1,14 @@
+package com.kdbk.fiszki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PanelKits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel_kits);
+    }
+}
--- Fiszki/app/src/main/java/com/kdbk/fiszki/YourProfile.java
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/YourProfile.java
@@ -3,15 +3,32 @@ package com.kdbk.fiszki;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class YourProfile extends AppCompatActivity {
 
     NextActivity nextActivity = new NextActivity(this);
+    private Button yoursKitsPanel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_profile);
+        setID();
+
+        yoursKitsPanel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextActivity.openActivity(PanelKits.class);
+            }
+        });
+    }
+
+    private void setID() {
+        yoursKitsPanel = findViewById(R.id.buttonYourFlashcardsProfile);
     }
 }
