490a77b7bfdaff57d92c542ff31678c40d39b37e
DwiN3 <dwinicht333@gmail.com>
2023-04-06 19:36:27 +0200

    Dodanie layoutu do dodawania fiszek

---
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/AddFlashcard.java
@@ -0,0 +1,14 @@
+package com.kdbk.fiszki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddFlashcard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_flashcard);
+    }
+}
