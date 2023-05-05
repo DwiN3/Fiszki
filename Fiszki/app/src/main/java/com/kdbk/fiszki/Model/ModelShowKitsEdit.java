cc729d764c823622ba819e65e26b8387d86f1e97
DwiN3 <dwinicht333@gmail.com>
2023-05-05 23:12:04 +0200

    Dodanie ponownie projektu

---
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/Model/ModelShowKitsEdit.java
@@ -0,0 +1,36 @@
+package com.kdbk.fiszki.Model;
+
+public class ModelShowKitsEdit {
+
+    private int ID;
+
+    private String word, translateWord,sentens,sentensTranslate;
+
+    public ModelShowKitsEdit(String word, String translateWord, String sentens, String sentensTranslate, int ID){
+        this.word = word;
+        this.translateWord = translateWord;
+        this.sentens = sentens;
+        this.sentensTranslate = sentensTranslate;
+        this.ID = ID;
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+    public String getWord() {
+        return word;
+    }
+
+    public String getTranslateWord() {
+        return translateWord;
+    }
+
+    public String getSentens() {
+        return sentens;
+    }
+
+    public String getSentensTranslate() {
+        return sentensTranslate;
+    }
+}
---
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/SelectListener/SelectListenerKits.java
@@ -0,0 +1,7 @@
+package com.kdbk.fiszki.SelectListener;
+
+import com.kdbk.fiszki.Model.ModelKits;
+
+public interface SelectListenerKits {
+    void onItemClicked(ModelKits modelKits);
+}
