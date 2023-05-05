47db17b2ae83636c575dffd487db88db7fee9cfc
DwiN3 <dwinicht333@gmail.com>
2023-05-05 13:37:54 +0200

    Działa ilość fiszek

--- Fiszki/app/src/main/java/com/kdbk/fiszki/Other/KitsArray.java
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/Other/KitsArray.java
@@ -10,7 +10,8 @@ public class KitsArray {
 
     private KitsArray() {
         list = new ArrayList<ModelKits>();
-        list.add(new ModelKits("Zestaw 1", "ILOSC FISZEK", "30", 1, 5));
+        WordsArray wordsArray = new WordsArray();
+        list.add(new ModelKits("Zestaw 1", "ILOSC FISZEK", String.valueOf(wordsArray.getLenOfArray()), 1, 5));
         list.add(new ModelKits("Zestaw 2", "ILOSC FISZEK", "18", 2, 31));
         list.add(new ModelKits("Zestaw 3", "ILOSC FISZEK", "25", 3, 21));
         list.add(new ModelKits("Zestaw 4", "ILOSC FISZEK", "30", 4, 3));
