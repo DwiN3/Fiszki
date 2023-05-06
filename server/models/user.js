07522ee20ef2dc4b1bf19411ba01bc86912680d3
q1bczyk <bartekkubik7@gmail.com>
2023-05-06 17:02:28 +0200

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,22 @@
-const model = 10
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new User({
+
+    email : {
+        type : String,
+        required : true
+    },
+
+    nick : {
+        type : String,
+        required : true
+    },
+
+    password : {
+        type : String,
+        required : true
+    }
+
+})
