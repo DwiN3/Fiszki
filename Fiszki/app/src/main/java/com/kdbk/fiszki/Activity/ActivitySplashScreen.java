6073b5cc3308e955922c32887527a839d426f407
DwiN3 <dwinicht333@gmail.com>
2023-05-10 22:11:42 +0200

    dodanie klasy splash activity

---
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivitySplashScreen.java
@@ -0,0 +1,16 @@
+package com.kdbk.fiszki.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kdbk.fiszki.R;
+
+public class ActivitySplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
