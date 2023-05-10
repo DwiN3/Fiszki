63eedb156a861148b7785bdccdb03df440eaf35f
DwiN3 <dwinicht333@gmail.com>
2023-05-10 22:50:12 +0200

    Dodanie sprawdzanie tokenu

--- Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivitySplashScreen.java
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivitySplashScreen.java
@@ -10,15 +10,29 @@ import com.kdbk.fiszki.R;
 
 public class ActivitySplashScreen extends AppCompatActivity {
 
+    private static final String TOKEN = "123123";
+    private Class<?> startScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        if (TOKEN.equals("123123")) {
+            startScreen = ActivityMainMenu.class;
+        } else {
+            startScreen = ActivityFirstScreen.class;
+        }
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(ActivitySplashScreen.this, ActivityFirstScreen.class));
+                if (startScreen != null) {
+                    Intent intent = new Intent(ActivitySplashScreen.this, startScreen);
+                    startActivity(intent);
+                } else {
+                    // Handle error
+                }
                 finish();
             }
         }, 2500);
