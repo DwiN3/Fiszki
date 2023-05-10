163fc9b24eef6556c7e9db53c3c27d5a78c51f09
DwiN3 <dwinicht333@gmail.com>
2023-05-10 22:17:12 +0200

    Dodanie splash screena

--- Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivitySplashScreen.java
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivitySplashScreen.java
@@ -2,7 +2,9 @@ package com.kdbk.fiszki.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.kdbk.fiszki.R;
 
@@ -12,5 +14,13 @@ public class ActivitySplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ActivitySplashScreen.this, ActivityFirstScreen.class));
+                finish();
+            }
+        }, 2500);
     }
 }
