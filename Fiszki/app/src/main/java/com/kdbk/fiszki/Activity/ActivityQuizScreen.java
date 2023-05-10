8fe445e89804236520d8aece1bf55d6c6b4e8ecf
DwiN3 <dwinicht333@gmail.com>
2023-05-10 18:28:14 +0200

    Działa pozostałe fiszki

--- Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivityQuizScreen.java
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivityQuizScreen.java
@@ -18,15 +18,23 @@ public class ActivityQuizScreen extends AppCompatActivity implements View.OnClic
     NextActivity nextActivity = new NextActivity(this);
     private Button next, exit;
     private ImageView imageWordQuiz;
-    private TextView answerText1, answerText2, answerText3, answerText4, nameWordQuizText;
+    private TextView answerText1, answerText2, answerText3, answerText4, nameWordQuizText, sticksLeftQuizText;
     private ImageButton answerButton1, answerButton2, answerButton3, answerButton4;
     private String selectedLanguage = "";
     private boolean isBackPressedBlocked = true; // zabezpieczenie na cofania poprzez klawisz wstecz
     private boolean markTheAnswer = false;
     private String correctAnswer;
+    private int nrWords = 3;
 
-    private int buttonCorrectAnswer = R.drawable.rounded_button_green;
-    private int buttonUnCorrectAnswer = R.drawable.rounded_button_red;
+
+    // TEST QUIZU
+    private int points = 0;
+    private String[] NameWord = {"Pies", "Niedzwiedz", "Krowa"};
+    private String[] ans1 = {"dog", "bird", "goat"};
+    private String[] ans2= {"duck", "bee", "cow"};
+    private String[] ans3= {"cat", "bear", "lion"};
+    private String[] ans4= {"worm", "bat", "sheep"};
+    private String[] correctANS = {"dog", "bear", "cow"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +51,7 @@ public class ActivityQuizScreen extends AppCompatActivity implements View.OnClic
         answerButton2.setOnClickListener(this);
         answerButton3.setOnClickListener(this);
         answerButton4.setOnClickListener(this);
-        setQuestion();
+        setQuestion(0);
     }
 
     @Override
@@ -54,27 +62,25 @@ public class ActivityQuizScreen extends AppCompatActivity implements View.OnClic
         return super.dispatchKeyEvent(event);
     }
 
-    void setQuestion(){
+    void setQuestion(int numberWord){
         markTheAnswer = false;
-        nameWordQuizText.setText("Pies");
-        answerText1.setText("dog");
-        answerText2.setText("duck");
-        answerText3.setText("cat");
-        answerText4.setText("worm");
-        correctAnswer = "dog";
-
+        sticksLeftQuizText.setText(""+nrWords);
+        nameWordQuizText.setText(NameWord[numberWord]);
+        answerText1.setText(ans1[numberWord]);
+        answerText2.setText(ans2[numberWord]);
+        answerText3.setText(ans3[numberWord]);
+        answerText4.setText(ans4[numberWord]);
+        correctAnswer = correctANS[numberWord];
         imageWordQuiz.setBackgroundResource(R.drawable.flagpl);
     }
 
-    void checkAnswer(){
-
-    }
 
     public void onClick(View view) {
 
         switch (view.getId()) {
             case R.id.imageButtonAnswerQuiz1:
                 if(answerText1.getText().equals(correctAnswer) && !markTheAnswer) {
+                    points +=10;
                     answerButton1.setBackgroundResource(R.drawable.rounded_button_green);
                     markTheAnswer = true;
                 } else if (!markTheAnswer) {
@@ -90,6 +96,7 @@ public class ActivityQuizScreen extends AppCompatActivity implements View.OnClic
                 break;
             case R.id.imageButtonAnswerQuiz2:
                 if(answerText2.getText().equals(correctAnswer) && !markTheAnswer) {
+                    points +=10;
                     answerButton2.setBackgroundResource(R.drawable.rounded_button_green);
                     markTheAnswer = true;
                 } else if (!markTheAnswer) {
@@ -105,6 +112,7 @@ public class ActivityQuizScreen extends AppCompatActivity implements View.OnClic
                 break;
             case R.id.imageButtonAnswerQuiz3:
                 if(answerText3.getText().equals(correctAnswer) && !markTheAnswer) {
+                    points +=10;
                     answerButton3.setBackgroundResource(R.drawable.rounded_button_green);
                     markTheAnswer = true;
                 } else if (!markTheAnswer) {
@@ -120,6 +128,7 @@ public class ActivityQuizScreen extends AppCompatActivity implements View.OnClic
                 break;
             case R.id.imageButtonAnswerQuiz4:
                 if(answerText4.getText().equals(correctAnswer) && !markTheAnswer) {
+                    points +=10;
                     answerButton4.setBackgroundResource(R.drawable.rounded_button_green);
                     markTheAnswer = true;
                 } else if (!markTheAnswer) {
@@ -134,12 +143,24 @@ public class ActivityQuizScreen extends AppCompatActivity implements View.OnClic
                 }
                 break;
             case R.id.buttonNextQuiz:
-                nextActivity.openActivity(ActivityQuizEnd.class);
+                nrWords -=1;
+                if(nrWords > 0){
+                    clearButtons();
+                    setQuestion(nrWords);
+                }
+                else nextActivity.openActivity(ActivityQuizEnd.class);
                 break;
             case R.id.buttonExitQuiz:
                 nextActivity.openActivity(ActivityMainMenu.class);
                 break;
         }
+    }
+
+    public void clearButtons(){
+        answerButton1.setBackgroundResource(R.drawable.rounded_button);
+        answerButton2.setBackgroundResource(R.drawable.rounded_button);
+        answerButton3.setBackgroundResource(R.drawable.rounded_button);
+        answerButton4.setBackgroundResource(R.drawable.rounded_button);
     }
 
 
@@ -156,5 +177,6 @@ public class ActivityQuizScreen extends AppCompatActivity implements View.OnClic
         answerText4 = findViewById(R.id.textAnswerQuiz4);
         nameWordQuizText = findViewById(R.id.nameWordQuiz);
         imageWordQuiz = findViewById(R.id.imageWordQuiz);
+        sticksLeftQuizText = findViewById(R.id.sticksLeftQuiz);
     }
 }
