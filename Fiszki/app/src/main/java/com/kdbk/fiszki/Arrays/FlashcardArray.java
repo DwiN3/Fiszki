5de0163f343fa90bda900667bf70c85b810e1e35
DwiN3 <dwinicht333@gmail.com>
2023-05-18 12:07:28 +0200

    zmiennienie domyślnej wartości zestawu

--- Fiszki/app/src/main/java/com/kdbk/fiszki/Arrays/FlashcardArray.java
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/Arrays/FlashcardArray.java
@@ -10,7 +10,7 @@ public class FlashcardArray {
     private ArrayList<ModelAddFlashcard> list = new ArrayList<>();
 
     private FlashcardArray() {
-        list.add(new ModelAddFlashcard(R.drawable.arrow, "NUMER ZESTAWU", "1", 1));
+        list.add(new ModelAddFlashcard(R.drawable.arrow, "NUMER ZESTAWU", "", 1));
         list.add(new ModelAddFlashcard(R.drawable.flagpl, "WPROWADŹ SŁOWO:", "", 2));
         list.add(new ModelAddFlashcard(R.drawable.flagang, "DODAJ TŁUMACZENIE:", "", 3));
         list.add(new ModelAddFlashcard(R.drawable.flagpl, "PRZYKŁADOWE ZDANIE", "", 4));
