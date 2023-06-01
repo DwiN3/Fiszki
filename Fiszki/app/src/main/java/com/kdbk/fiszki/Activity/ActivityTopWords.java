a6824214cfd6d34dd5f07f084f2990ae11a86a41
DwiN3 <dwinicht333@gmail.com>
2023-06-01 22:22:20 +0200

    dodanie top słówek

---
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivityTopWords.java
@@ -0,0 +1,16 @@
+package com.kdbk.fiszki.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kdbk.fiszki.R;
+
+public class ActivityTopWords extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_words);
+    }
+}
