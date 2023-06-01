e0de78466fd6c92adcf26d7554ebbd2c02fe967d
DwiN3 <dwinicht333@gmail.com>
2023-06-02 01:09:11 +0200

    Dodanie losowego zestawu

--- Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivityTopWords.java
+++ Fiszki/app/src/main/java/com/kdbk/fiszki/Activity/ActivityTopWords.java
@@ -16,15 +16,19 @@ import com.kdbk.fiszki.Other.NextActivity;
 import com.kdbk.fiszki.R;
 import com.kdbk.fiszki.RecyclerView.Adaper.AdapterShowKitsEdit;
 import com.kdbk.fiszki.RecyclerView.Adaper.AdapterTopWords;
+import com.kdbk.fiszki.RecyclerView.Model.ModelKits;
 import com.kdbk.fiszki.RecyclerView.Model.ModelShowKitsEdit;
 import com.kdbk.fiszki.RecyclerView.SelectListener.SelectListenerShowKitsEdit;
 import com.kdbk.fiszki.RecyclerView.SelectListener.SelectListenerTopWords;
 import com.kdbk.fiszki.Retrofit.JsonPlaceholderAPI.JsonFlashcardsCollections;
+import com.kdbk.fiszki.Retrofit.Models.FlashcardCollections;
 import com.kdbk.fiszki.Retrofit.Models.FlashcardCollectionsWords;
 import com.kdbk.fiszki.Retrofit.Models.FlashcardID;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -43,6 +47,8 @@ public class ActivityTopWords extends AppCompatActivity implements SelectListene
     private RecyclerView.LayoutManager mLayoutManager;
     private Button back;
     private ArrayList<ModelShowKitsEdit> wordsList = new ArrayList<>();
+    private ArrayList<ModelKits> collectionList = new ArrayList<>();
+    private String listToShow="";
     private boolean isBackPressedBlocked = true;
 
     @Override
@@ -50,12 +56,62 @@ public class ActivityTopWords extends AppCompatActivity implements SelectListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_words);
         setID();
-        showWords();
+        getKitName();
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 nextActivity.openActivity(ActivityGameMode.class);
+            }
+        });
+    }
+
+    private void getKitName() {
+
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request newRequest = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer " + tokenInstance.getToken())
+                        .build();
+                return chain.proceed(newRequest);
+            }
+        }).build();
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://flashcard-app-api-bkrv.onrender.com/api/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonFlashcardsCollections jsonFlashcardsCollections = retrofit.create(JsonFlashcardsCollections.class);
+        Call<List<FlashcardCollections>> call = jsonFlashcardsCollections.getAllFlashcardsCollections();
+
+        call.enqueue(new Callback<List<FlashcardCollections>>() {
+            @Override
+            public void onResponse(Call<List<FlashcardCollections>> call, Response<List<FlashcardCollections>> response) {
+                collectionList.clear();
+                List<FlashcardCollections> list = response.body();
+                if (list == null || list.isEmpty()) {
+                    return;
+                }
+                else{
+                    int id = 0;
+                    for (FlashcardCollections collection : list) {
+
+                        collectionList.add(new ModelKits(collection.getCollectionName(), "ILOSC FISZEK", collection.getFlashcardsSize(), id, 30, collection.getId()));
+                        id++;
+                    }
+                    Random random = new Random();
+                    int randomNumber = random.nextInt(list.size()) + 1;
+                    listToShow = collectionList.get(randomNumber).getNameKit();
+                    showWords();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<FlashcardCollections>> call, Throwable t) {
+                if(t.getMessage().equals("timeout"))  Toast.makeText(ActivityTopWords.this,"Uruchamianie serwera", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -80,7 +136,7 @@ public class ActivityTopWords extends AppCompatActivity implements SelectListene
                 .build();
 
         JsonFlashcardsCollections jsonFlashcardsCollections = retrofit.create(JsonFlashcardsCollections.class);
-        Call<FlashcardCollectionsWords> call = jsonFlashcardsCollections.getKit("zwierzeta");
+        Call<FlashcardCollectionsWords> call = jsonFlashcardsCollections.getKit(listToShow);
 
         call.enqueue(new Callback<FlashcardCollectionsWords>() {
             @Override
@@ -112,6 +168,8 @@ public class ActivityTopWords extends AppCompatActivity implements SelectListene
             }
         });
     }
+
+
 
 
     private void RefreshRecycleView() {
